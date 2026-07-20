777fa412c64f9ca57eec3a043be6f38232d94a0d
Ahmed Kandil <a.akandil@outlook.com>
2026-07-20 20:17:25 +0200

    Plot corrected TSPLIB scaling results

--- script/plot_speedup.m
+++ script/plot_speedup.m
@@ -1,16 +1,19 @@
 graphics_toolkit("gnuplot");
-data = dlmread('scaling_results.csv', ',', 1, 0);
-cores = data(:, 1); time = data(:, 2);
+data = dlmread('scaling_results_tsplib_euc2d.csv', ',', 1, 0);
+cores = data(:, 1);
+time = data(:, 3);
 speedup = time(1) ./ time;
 efficiency = (speedup ./ cores) * 100;
 
 figure('visible', 'off');
 plot(cores, speedup, '-bo', 'LineWidth',2, 'MarkerFaceColor','b'); hold on;
 plot(cores, cores, '--k', 'LineWidth',1);
-xlabel('Processors'); ylabel('Speedup'); title('Speedup'); grid on;
-print('scaling_speedup.png', '-dpng');
+xlabel('Processors'); ylabel('Speedup');
+title('Speedup: TSPLIB EUC\_2D Benchmark'); grid on;
+print('scaling_speedup_tsplib_euc2d.png', '-dpng');
 
 figure('visible', 'off');
 plot(cores, efficiency, '-s', 'LineWidth',2, 'Color',[0,0.5,0], 'MarkerFaceColor','g');
 xlabel('Processors'); ylabel('Efficiency (%)'); ylim([0, 110]); grid on;
-print('scaling_efficiency.png', '-dpng');
+title('Parallel Efficiency: TSPLIB EUC\_2D Benchmark');
+print('scaling_efficiency_tsplib_euc2d.png', '-dpng');
